33722f24012f53617fa222cb4e5bba4f180c2415
fortuna <fortuna@localhost>
2010-07-31 17:03:06 +1000

    Remove ambiguity on javax.jcr.Node

--- src/main/groovy/org/mnode/juicer/JcrNodeCategory.groovy
+++ src/main/groovy/org/mnode/juicer/JcrNodeCategory.groovy
@@ -1,19 +1,18 @@
 package org.mnode.juicer
 
-import javax.jcr.Node;
 import javax.jcr.NodeIterator;
 
 class JcrNodeCategory {
 
 //    static def propertyMissing(Node node, String propertyName) {
-    static javax.jcr.Property get(Node node, String propertyName) {
+    static javax.jcr.Property get(javax.jcr.Node node, String propertyName) {
         if (node.hasProperty(propertyName)) {
             return node.getProperty(propertyName)
         }
         return null
     }
     
-    static Node getAt(Node node, int index) {
+    static javax.jcr.Node getAt(javax.jcr.Node node, int index) {
         if (node.nodes.size() >= index - 1) {
             NodeIterator children = node.nodes
             children.skip index
